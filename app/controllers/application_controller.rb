1ffa417969ff8813def829ed8bcb751665628953
quyenguyengoc <quyenguyengoc911@gmail.com>
2016-10-03 22:06:03 +0700

    login-logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
+
+  protected
+
+  def verify_access
+    unless verify_user_access?
+      flash[:danger] = t "require_login"
+      redirect_to root_url
+    end
+  end
 end
