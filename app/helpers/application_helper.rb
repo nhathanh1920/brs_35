9c7eee4efd1c52149d7b46d927ed00043ffe1b04
hanhttm96 <myhanhqt96@>
2016-10-03 10:08:51 +0700

    edit-staticpage-user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def full_title page_title = ""
-    base_title = t "base_title"
+    base_title = t "project_name"
     if page_title.empty?
       base_title
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-
   namespace :admin do
     root "static_pages#home"
   end
-
   root "static_pages#home"
-  get "static_pages/help"
 end
