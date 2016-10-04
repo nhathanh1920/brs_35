902f2dd0b96c1951abac858a521bc109c257dea5
Hoang Nhat <nguyenhoangnhat07@gmail.com>
2016-10-04 15:23:28 +0700

    Show Static_page

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,6 @@ class Book < ApplicationRecord
   has_many :reading_books
   has_many :rates
   has_many :reviews
+
+  scope :newest, ->{order created_at: :desc}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,43 @@ User.create! name: "User", email: "user@gmail.com",
   name  = Faker::Name.name
   Category.create!(name: name)
 end
+
+Book.create! name: "book 1", description: "", image: "user/book1",
+  publish_date: "2015-10-10", author: "abc",  page: "100", rating: "3.5",
+  category_id: "1"
+Book.create! name: "book 1", description: "", image: "user/book1",
+  publish_date: "2015-10-10", author: "abc" , page: "100", rating: "3.5",
+  category_id: "1"
+Book.create! name: "book 1", description: "", image: "user/book1",
+  publish_date: "2015-10-10", author: "abc",  page: "100", rating: "3.5",
+  category_id: "1"
+Book.create! name: "book 1", description: "", image: "user/book1",
+  publish_date: "2015-10-10", author: "abc" , page: "100", rating: "3.5",
+  category_id: "1"
+Book.create! name: "book 2", description: "", image: "user/book2",
+  publish_date: "2015-10-10", author: "abc",  page: "100", rating: "3.5",
+  category_id: "2"
+Book.create! name: "book 2", description: "", image: "user/book2",
+  publish_date: "2015-10-10", author: "abc",  page: "100", rating: "3.5",
+  category_id: "2"
+Book.create! name: "book 2", description: "", image: "user/book2",
+  publish_date: "2015-10-10", author: "abc",  page: "100", rating: "3.5",
+  category_id: "2"
+Book.create! name: "book 2", description: "", image: "user/book2",
+  publish_date: "2015-10-10", author: "abc",  page: "100", rating: "3.5",
+  category_id: "2"
+Book.create! name: "book 3", description: "", image: "user/book3",
+  publish_date: "2015-10-10", author: "abc",  page: "100", rating: "3.5",
+  category_id: "3"
+Book.create! name: "book 3", description: "", image: "user/book3",
+  publish_date: "2015-10-10", author: "abc",  page: "100", rating: "3.5",
+  category_id: "3"
+Book.create! name: "book 3", description: "", image: "user/book3",
+  publish_date: "2015-10-10", author: "abc",  page: "100", rating: "3.5",
+  category_id: "3"
+Book.create! name: "book 3", description: "", image: "user/book3",
+  publish_date: "2015-10-10", author: "abc",  page: "100", rating: "3.5",
+  category_id: "3"
+Book.create! name: "book 4", description: "", image: "user/book4",
+  publish_date: "2015-10-10", author: "abc",  page: "100", rating: "3.5",
+  category_id: "4"
