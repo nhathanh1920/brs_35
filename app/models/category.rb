5f32498508882918756ef6fb359560406ad397fb
quyenguyengoc <quyenguyengoc911@gmail.com>
2016-10-04 15:11:38 +0700

    finish list categories for admin section

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
+  scope :newest, ->{order created_at: :desc}
+  validates :name, presence: true, length: {maximum: 50}, uniqueness: true
   has_many :books, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
     root "static_pages#home"
+    resources :categories
   end
   root "static_pages#home"
   get "/login", to: "sessions#new"
