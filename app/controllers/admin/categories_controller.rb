219ef87eb6931d1f7fa4ae428693a842012640c3
quyenguyengoc <quyenguyengoc911@gmail.com>
2016-10-06 23:16:54 +0700

    finish search-and-update-category-admin-section

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,11 @@
 class Admin::CategoriesController < ApplicationController
   before_action :verify_access
+  before_action :find_categories, only: [:edit, :destroy, :update]
   layout "admin_application"
 
   def index
-    @categories = Category.newest.paginate page: params[:page],
-      per_page: Settings.per_page
+    @categories = Category.search(params[:search]).newest.paginate page:
+      params[:page], per_page: Settings.per_page
     @category = Category.new
   end
 
@@ -16,8 +17,22 @@ class Admin::CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    unless @category
+      redirect_to admin_categories_path
+    end
+  end
+
+  def update
+    if @category.update_attributes category_params
+      flash[:info] = t "alert_result"
+      redirect_to admin_categories_path
+    else
+      render :edit
+    end
+  end
+
   def destroy
-    @category = Category.find_by id: params[:id]
     unless @category
       respond_to do |format|
         format.js {render "fail_update_or_del.js.erb"}
@@ -33,5 +48,9 @@ class Admin::CategoriesController < ApplicationController
   private
   def category_params
     params.require(:category).permit :name
+  end
+
+  def find_categories
+    @category = Category.find_by id: params[:id]
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,14 @@ class Category < ApplicationRecord
   scope :newest, ->{order created_at: :desc}
   validates :name, presence: true, length: {maximum: 50}, uniqueness: true
   has_many :books, dependent: :destroy
+
+  class << self
+    def search search
+      if search
+        where "name LIKE ?", "%#{search}%"
+      else
+        newest
+      end
+    end
+  end
 end
