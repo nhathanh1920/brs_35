3364cbb4667cf03fda3e729294700f068e3d4923
hanhttm96 <myhanhqt96@>
2016-10-06 09:17:29 +0700

    refactor code sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  layout false, only: [:new]
+
   def new
     @user = User.new
   end
@@ -6,11 +8,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       redirect_to root_path
     else
       render :new
     end
   end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
