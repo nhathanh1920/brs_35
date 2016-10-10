98f4f23f6266dcbf58c7e210bff227cc8f2b5dfe
quyenguyengoc <quyenguyengoc911@gmail.com>
2016-10-11 00:05:09 +0700

    refactor home-page user section

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,18 +2,6 @@ class StaticPagesController < ApplicationController
   before_action :load_categories
 
   def home
-    if params[:category].blank?
-      @books = Book.search(params[:search]).newest.paginate page: params[:page],
-        per_page: Settings.per_page
-    else
-      @category = Category.find_by id: params[:category]
-      if @category.nil?
-        flash[:danger] = t "category_nil"
-        redirect_to root_path
-      else
-        @books = @category.books.paginate page: params[:page],
-        per_page: Settings.per_page
-      end
-    end
+    @books = Book.hottest
   end
 end
